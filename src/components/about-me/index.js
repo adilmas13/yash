382a7b800d7be9f49be67e444482a9e46a40cda6
Adil Shaikh <adil@invideo.io>
2021-05-29 22:50:57 +0530

    feat: main screen of about me mobile view - wip

--- src/components/about-me/index.js
+++ src/components/about-me/index.js
@@ -1,10 +1,69 @@
 import style from './style.css';
-import {useEffect, useState} from "preact/hooks";
+import {useEffect, useRef, useState} from "preact/hooks";
 import MyInfo from "../my-info";
 import Experience from "../experience";
 import Literacy from "../literacy";
+import Logo from "../logo";
 
-const AboutMe = () => {
+const MobileView = () => {
+    const [pageNo, setPageNo] = useState(0);
+    const nextArrowRef = useRef(null);
+    const previousArrowRef = useRef(null);
+
+    useEffect(() => {
+        const element = document.getElementById("scroller");
+        element.scroll({
+            top: (element.clientHeight) * pageNo,
+            behavior: "smooth"
+        })
+        return () => {
+        };
+    }, [pageNo]);
+
+    useEffect(() => {
+        const downArrow = nextArrowRef.current;
+        const upArrow = previousArrowRef.current;
+
+        if (pageNo === 2 && downArrow) {
+            downArrow.animate([
+                {transform: 'scale(1)'},
+                {transform: 'scale(0)'},
+            ], {duration: 200, fill: "forwards"}).play()
+        }
+        if (pageNo === 1 && upArrow) {
+            upArrow.animate([
+                {transform: 'scale(0) translateX(-50%)'},
+                {transform: 'scale(1) translateX(-50%)'},
+            ], {duration: 200, fill: "forwards"}).play()
+        }
+    }, [pageNo]);
+
+    const onPrevClicked = () => setPageNo((currentPageNo) => currentPageNo - 1);
+    const onNextClicked = () => setPageNo((currentPageNo) => currentPageNo + 1);
+
+    return <div className={style.parent}>
+        <div className={style['scroll-container']} id="scroller">
+            <MyInfo />
+            <Experience />
+            <Literacy />
+        </div>
+        <div className={style['top-arrow-container']}>
+            <Logo />
+            <div className={style['icon-wrapper']} onClick={onPrevClicked} ref={previousArrowRef}>
+                <div>next</div>
+                <img src={'assets/arrow_blunt.svg'} />
+            </div>
+        </div>
+        <div className={style['bottom-arrow-container']}>
+            <div className={style['icon-wrapper']} onClick={onNextClicked} ref={nextArrowRef}>
+                <div>next</div>
+                <img src={'assets/arrow_blunt.svg'} />
+            </div>
+        </div>
+    </div>
+}
+
+const DesktopView = () => {
     const [pageNo, setPageNo] = useState(0);
 
     useEffect(() => {
@@ -32,5 +91,14 @@ const AboutMe = () => {
         </div>
     </div>
 }
+
+const AboutMe = () => <div>
+    <div className={style.mobile}>
+        <MobileView />
+    </div>
+    <div className={style.desktop}>
+        <DesktopView />
+    </div>
+</div>
 
 export default AboutMe;
