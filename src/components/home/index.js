c81d618ee6edc4b364d936e80ca4e3ef9c9ee006
Adil Shaikh <adil@invideo.io>
2021-05-31 21:45:13 +0530

    fix: test

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -19,7 +19,7 @@ import {
 import {downArrow, upArrow} from "./utils";
 
 const useIsMobileView = () => {
-    const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
+    const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 701);
     const breakPoint = 700;
     useEffect(() => {
         const handleWindowResize = () => setWidth(window.innerWidth);
