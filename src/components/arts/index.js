dac7c167b28b55f588305c565f08314109d6ed79
Adil Shaikh <adil@invideo.io>
2021-06-10 21:38:16 +0530

    feat: arts responsive

--- src/components/arts/index.js
+++ src/components/arts/index.js
@@ -1,14 +1,89 @@
 // eslint-disable-next-line no-unused-vars
 import React from "preact";
 import CommonListing from "../common";
-import {arts} from "../../utils/dataService";
+import {adverts, arts, artss} from "../../utils/dataService";
+import {useEffect, useState} from "preact/hooks";
+import CommonListing2 from "../common2";
+
+const breakpoints = [
+    {
+        point: 400,
+        noOfColumns: 3,
+        data: artss
+            .filter(it => it.id !== "blank")
+            .sort((a, b) => a.id - b.id)
+    }, {
+        point: 800,
+        noOfColumns: 4,
+        data: artss
+            .filter(it => it.id !== "blank")
+            .sort((a, b) => a.id - b.id)
+    }, {
+        point: 1200,
+        noOfColumns: 5,
+        data: artss
+    }];
+
+const getDataBasedOnBreakPoint = () => {
+    const width = window.innerWidth;
+    return breakpoints.reduce((acc, value) => {
+        if (width > value.point)
+            return value;
+        return acc;
+    }, breakpoints[0]);
+};
+
+const useAdvertsData = () => {
+    const [data, setData] = useState([]);
+    const [breakPoint, setBreakPoint] = useState(getDataBasedOnBreakPoint());
+
+    useEffect(() => {
+        let timeOut;
+        const handleWindowResize = () => {
+            if (timeOut) {
+                clearTimeout(timeOut);
+            }
+            timeOut = setTimeout(() => {
+                setBreakPoint(Object.assign({}, getDataBasedOnBreakPoint()));
+            }, 1000);
+        };
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, [])
+
+    useEffect(() => {
+        const containerWidth = document.getElementById('scroll-container').offsetWidth;
+        const noOfColumns = breakPoint.noOfColumns;
+        const gap = 10;
+        const columnWidth = ((containerWidth - (gap * (noOfColumns + 1))) / noOfColumns);
+        const final = breakPoint.data
+            .reduce((accumulator, value, index) => {
+                const temp = index % noOfColumns;
+                const topIndex = (index - noOfColumns);
+                const aspect = value.ratio.split(":");
+                const topGap = value.id !== "blank" ? gap : 0;
+                const data = {
+                    top: topIndex > -1 ? accumulator[topIndex].height + accumulator[topIndex].top + topGap : 0,
+                    height: value.id !== "blank" ? columnWidth / (aspect[0] / aspect[1]) : 0,
+                    left: (columnWidth * temp) + (gap * (temp + 1)),
+                    width: columnWidth,
+                    media: value
+                }
+                return [...accumulator, ...[data]]
+            }, [])
+
+        setData(final);
+    }, [breakPoint])
+    return data;
+}
 
 const Arts = () => {
-    const config = {
-        data: arts,
+    const data = useAdvertsData();
+
+    return <CommonListing2 config={{
+        data,
         type: "arts"
-    }
-    return <CommonListing config={config} />
+    }} />
 };
 
 export default Arts;
--- src/utils/dataService.js
+++ src/utils/dataService.js
@@ -675,7 +675,7 @@ export const adverts = [
         id: 37
     },
     {
-        image:"",
+        image: "",
         ratio: "",
         column: -1,
         groupId: "",
@@ -703,7 +703,7 @@ export const adverts = [
         id: 41
     },
     {
-        image:"",
+        image: "",
         ratio: "",
         column: -1,
         groupId: "",
@@ -732,14 +732,14 @@ export const adverts = [
         id: 45
     },
     {
-        image:"",
+        image: "",
         ratio: "",
         column: -1,
         groupId: "",
         id: "blank"
     },
     {
-        image:"",
+        image: "",
         ratio: "",
         column: -1,
         groupId: "",
@@ -754,3 +754,349 @@ export const adverts = [
         id: 48
     }
 ];
+
+export const artss = [
+    {
+        id: 1,
+        image: "bumble1",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 2,
+        image: "bumble6",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 13,
+        image: "bumble2",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 14,
+        image: "bumble7",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 25,
+        image: "bumble3",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 26,
+        image: "bumble8",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 36,
+        image: "bumble4",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 37,
+        image: "bumble9",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 47,
+        image: "bumble5",
+        groupId: 1,
+        ratio: "9:16"
+    },
+    {
+        id: 3,
+        image: "park3",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 4,
+        image: "park8",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 15,
+        image: "park4",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 16,
+        image: "park9",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 27,
+        image: "park5",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 28,
+        image: "park10",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 38,
+        image: "park6",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 48,
+        image: "park1",
+        groupId: 2,
+        ratio: "16:9",
+        videoId: "mRM_W7_eN6I"
+    },
+    {
+        id: 49,
+        image: "park7",
+        groupId: 2,
+        ratio: "9:16"
+    },
+    {
+        id: 6,
+        image: "nba8",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 7,
+        image: "nba13",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 18,
+        image: "nba9",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 29,
+        image: "nba5",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 30,
+        image: "nba10",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 39,
+        image: "nba1",
+        groupId: 3,
+        ratio: "16:9"
+    },
+    {
+        id: 17,
+        image: "nba4",
+        groupId: 3,
+        ratio: "16:9"
+    },
+    {
+        id: 40,
+        image: "nba6",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 41,
+        image: "nba11",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 50,
+        image: "nba2",
+        groupId: 3,
+        ratio: "16:9"
+    },
+    {
+        id: 5,
+        image: "nba3",
+        groupId: 3,
+        ratio: "16:9"
+    },
+    {
+        id: 51,
+        image: "nba7",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 52,
+        image: "nba12",
+        groupId: 3,
+        ratio: "9:16"
+    },
+    {
+        id: 19,
+        image: "tc1",
+        groupId: 4,
+        ratio: "9:16"
+    },
+    {
+        id: 31,
+        image: "tc2",
+        groupId: 4,
+        ratio: "9:16"
+    },
+    {
+        id: 42,
+        image: "tc3",
+        groupId: 4,
+        ratio: "9:16"
+    },
+    {
+        id: 53,
+        image: "tc4",
+        groupId: 4,
+        ratio: "9:16"
+    },
+    {
+        id: 8,
+        image: "gc1",
+        groupId: 5,
+        ratio: "9:16"
+    },
+    {
+        id: 20,
+        image: "gc2",
+        groupId: 5,
+        ratio: "9:16"
+    },
+    {
+        id: 32,
+        image: "gc3",
+        groupId: 5,
+        ratio: "9:16"
+    },
+    {
+        id: 43,
+        image: "gc4",
+        groupId: 5,
+        ratio: "9:16"
+    },
+    {
+        id: 54,
+        image: "gc5",
+        groupId: 5,
+        ratio: "9:16"
+    },
+    {
+        id: 9,
+        image: "axn1",
+        groupId: 6,
+        ratio: "9:16"
+    },
+    {
+        id: 10,
+        image: "axn6",
+        groupId: 6,
+        ratio: "16:9"
+    },
+    {
+        id: 21,
+        image: "axn2",
+        groupId: 6,
+        ratio: "16:9"
+    },
+    {
+        id: 22,
+        image: "axn7",
+        groupId: 6,
+        ratio: "9:16"
+    },
+    {
+        id: 33,
+        image: "axn3",
+        groupId: 6,
+        ratio: "16:9"
+    },
+    {
+        id: 44,
+        image: "axn4",
+        groupId: 6,
+        ratio: "16:9"
+    },
+    {
+        id: 55,
+        image: "axn5",
+        groupId: 6,
+        ratio: "9:16"
+    },
+    {
+        id: 11,
+        image: "goomo4",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 23,
+        image: "goomo5",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 34,
+        image: "goomo1",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 35,
+        image: "goomo6",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 45,
+        image: "goomo2",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 46,
+        image: "goomo7",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 56,
+        image: "goomo3",
+        groupId: 7,
+        ratio: "9:16"
+    },
+    {
+        id: 12,
+        image: "mcd2",
+        groupId: 8,
+        ratio: "16:9"
+    },
+    {
+        id: 24,
+        image: "mcd3",
+        groupId: 8,
+        ratio: "9:16"
+    },
+    {
+        id: 57,
+        image: "mcd1",
+        groupId: 8,
+        ratio: "16:9"
+    }
+]
