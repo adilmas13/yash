7160cec3ccba00168f710586cdb8580d5661eda2
Adil Shaikh <adil@invideo.io>
2021-06-10 22:08:27 +0530

    chore: removes lottie

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,20 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "preact";
 import style from './style.css';
-import Lottie from 'react-lottie';
-import animation from '../../assets/data.json'
 
 const Loader = () => {
 
-    const bodymovinOptions = {
-        loop: true,
-        autoplay: true,
-        prerender: true,
-        animationData: animation
-    }
-
     return <div class={style.parent}>
-        <div class={style["loader"]}><Lottie options={bodymovinOptions} /></div>
+        <div class={style["loader"]}>LOADING...</div>
     </div>
 };
 
