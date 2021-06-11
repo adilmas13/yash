e6f3fa6e0628c13fc455de47a917fd0614a40c6c
Adil Shaikh <adil@invideo.io>
2021-06-12 01:23:25 +0530

    fix: broken awards description

--- src/components/awards/index.js
+++ src/components/awards/index.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import {React} from "preact";
 import style from './style.css';
-import {awardsDescription, awardsOriginal, awardsThumbnails} from "../../dataSource/awards";
-import {awardsThumbnail} from "../../service/imgService";
+import {awardsDescription, awardsDetails, awardsOriginal, awardsThumbnails} from "../../dataSource/awards";
 import {useState} from "preact/hooks";
 import Logo from "../logo";
 import Preview from "../preview";
 import Description from "../description";
 
 const MediaCell = (props) => {
+    const media = props.media;
+
     const [isImageLoaded, setImageLoaded] = useState(false);
 
     let cellStyle = {
@@ -16,14 +17,14 @@ const MediaCell = (props) => {
     }
 
     let imageWrapperStyle = {
-        backgroundColor: `${props.media.color}33` || "lightgrey"
+        backgroundColor: `${media.color}33` || "lightgrey"
     }
 
     return <div class={style["image-wrapper"]} style={imageWrapperStyle}>
         <img
             class={style.cell}
             style={cellStyle}
-            src={awardsThumbnail(props.media.image)}
+            src={media.thumbnail}
             alt="image"
             onLoad={() => setImageLoaded(true)}
             onClick={() => props.onClicked()}
@@ -32,20 +33,18 @@ const MediaCell = (props) => {
 };
 
 const Awards = () => {
-        const [previewMedia, setPreviewMedia] = useState(undefined);
-        const [selectedDescription, setSelectedDescription] = useState(undefined);
+        const [preview, setPreview] = useState(undefined);
+        const [description, setDescription] = useState(undefined);
 
         const onClicked = (media) => {
-            const data = {
-                group: awardsOriginal.find(it => it.id === media.id).media,
-                selected: awardsOriginal.find(it => it.id === media.id).media[0],
-                type: "awards"
-            }
-            setPreviewMedia(data);
-            setSelectedDescription(awardsDescription.find(it => it.id === media.id));
+            setPreview({
+                group: awardsDetails.filter(it => it.groupId === media.id),
+                selected: 0,
+            });
+            setDescription(awardsDescription.find(it => it.id === media.id));
         };
 
-        const onDescriptionCloseClicked = () => setSelectedDescription(undefined);
+        const handleDescriptionBackClick = () => setDescription(undefined);
 
         return <div class={style.parent}>
             <Logo />
@@ -54,12 +53,12 @@ const Awards = () => {
                     {awardsThumbnails.map(data => <MediaCell media={data} onClicked={() => onClicked(data)} />)}
                 </div>
             </div>
-            {previewMedia &&
+            {preview &&
             <Preview
-                data={previewMedia}
-                onCancelClicked={() => setPreviewMedia(undefined)} />}
-            {selectedDescription &&
-            <Description data={selectedDescription} onCloseClicked={() => onDescriptionCloseClicked()} />}
+                data={preview}
+                handleBackClick={() => setPreview(undefined)} />}
+            {description &&
+            <Description data={description} onCloseClicked={handleDescriptionBackClick} />}
         </div>
     }
 ;
--- src/dataSource/awards.js
+++ src/dataSource/awards.js
@@ -1,4 +1,4 @@
-import {awardsLogo} from "../service/imgService";
+import {awardsOriginal, awardsLogo, awardsThumbnail} from "../service/imgService";
 
 export const awardsThumbnails = [
     {id: 1, image: "1jugnooH", color: "#f2fc75"},
@@ -12,7 +12,10 @@ export const awardsThumbnails = [
     {id: 9, image: "9hateH", color: "#771d1c"},
     {id: 10, image: "10jeepH", color: "#d3693b"},
     {id: 11, image: "11youngH", color: "#f2fc75"}
-]
+].map(it => {
+    it.thumbnail = awardsThumbnail(it.image);
+    return it;
+})
 
 export const awardsDescription = [
     {
@@ -88,90 +91,286 @@ export const awardsDescription = [
     return it;
 })
 
-export const awardsOriginal = [
-    {id: 1, media: [{image: "", videoId: "NRxbrwi4WTE", ratio: "16:9"}]},
+export const awardsDetails = [
     {
-        id: 2, media: [
-            {id: 1, image: "1thirdInside", ratio: "16:9"},
-            {id: 2, image: "2thirdInside", ratio: "16:9"},
-            {id: 3, image: "3thirdInside", ratio: "16:9"},
-            {id: 4, image: "4thirdInside", ratio: "16:9"},
-            {id: 5, image: "5thirdInside", ratio: "16:9"},
-            {id: 6, image: "6thirdInside", ratio: "16:9"},
-            {id: 7, image: "7thirdInside", ratio: "16:9"},
-            {id: 8, image: "8thirdInside", ratio: "16:9"},
-            {id: 9, image: "9thirdInside", ratio: "16:9"},
-            {id: 10, image: "10thirdInside", ratio: "16:9"},
-        ]
+        id: "1-1",
+        image_name: "",
+        videoId: "NRxbrwi4WTE",
+        ratio: "16:9",
+        groupId: 1
     },
     {
-        id: 3, media: [
-            {id: 1, image: "1paraInside", ratio: "16:9"},
-            {id: 2, image: "2paraInside", ratio: "16:9"},
-            {id: 3, image: "3paraInside", ratio: "16:9"},
-            {id: 4, image: "4paraInside", ratio: "16:9"},
-            {id: 5, image: "5paraInside", ratio: "16:9"},
-            {id: 6, image: "6paraInside", ratio: "16:9"},
-            {id: 7, image: "7paraInside", ratio: "16:9"},
-        ]
+        id: "2-1",
+        image_name: "1thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 4, media: [
-            {id: 1, image: "", videoId: "IhgTAt8Z3aY", ratio: "16:9"},
-        ]
+        id: "2-2",
+        image_name: "2thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 5, media: [
-            {id: 1, image: "", videoId: "YiGkVg9oUpA", ratio: "16:9"},
-        ]
+        id: "2-3",
+        image_name: "3thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 6, media: [
-            {id: 1, image: "", videoId: "HkA7oioz4b8", ratio: "16:9"},
-        ]
+        id: "2-4",
+        image_name: "4thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 7, media: [
-            {id: 1, image: "1loveinside", ratio: "16:9"},
-            {id: 2, image: "2loveinside", ratio: "16:9"},
-            {id: 3, image: "3loveinside", ratio: "16:9"},
-            {id: 4, image: "4loveinside", ratio: "16:9"},
-            {id: 5, image: "5loveinside", ratio: "16:9"},
-            {id: 6, image: "6loveinside", ratio: "16:9"},
-            {id: 7, image: "7loveinside", ratio: "16:9"},
-            {id: 8, image: "8loveinside", ratio: "16:9"},
-            {id: 9, image: "9loveinside", ratio: "16:9"},
-            {id: 10, image: "10loveinside", ratio: "16:9"},
-            {id: 11, image: "10loveinside", ratio: "16:9"},
-        ]
+        id: "2-5",
+        image_name: "5thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 8, media: [
-            {id: 1, image: "1mercyInside", ratio: "16:9"},
-            {id: 2, image: "2mercyInside", ratio: "16:9"},
-        ]
+        id: "2-6",
+        image_name: "6thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 9, media: [
-            {id: 1, image: "1hateinside", ratio: "16:9"},
-            {id: 2, image: "2hateinside", ratio: "16:9"},
-            {id: 3, image: "3hateinside", ratio: "16:9"},
-            {id: 4, image: "4hateinside", ratio: "16:9"},
-            {id: 5, image: "5hateinside", ratio: "16:9"},
-            {id: 6, image: "6hateinside", ratio: "16:9"},
-            {id: 7, image: "7hateinside", ratio: "16:9"},
-            {id: 8, image: "8hateinside", ratio: "16:9"},
-        ]
+        id: "2-7",
+        image_name: "7thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 10, media: [
-            {id: 1, image: "1jeepInside", ratio: "16:9"},
-        ]
+        id: "2-8",
+        image_name: "8thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
     {
-        id: 11, media: [
-            {id: 1, image: "", videoId: "XjAWoUc4Aj4", ratio: "16:9"},
-            {id: 2, image: "", videoId: "4txgs7qIgGo", ratio: "16:9"},
-        ]
+        id: "2-9",
+        image_name: "9thirdInside",
+        ratio: "16:9",
+        groupId: 2
     },
-]
+    {
+        id: "2-10",
+        image_name: "10thirdInside",
+        ratio: "16:9",
+        groupId: 2
+    },
+    {
+        id: "3-1",
+        image_name: "1paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-2",
+        image_name: "2paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-3",
+        image_name: "3paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-4",
+        image_name: "4paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-5",
+        image_name: "5paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-6",
+        image_name: "6paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "3-7",
+        image_name: "7paraInside",
+        ratio: "16:9",
+        groupId: 3
+    },
+    {
+        id: "4-1",
+        image_name: "",
+        videoId: "IhgTAt8Z3aY",
+        ratio: "16:9",
+        groupId: 4
+    },
+    {
+        id: "5-1",
+        image_name: "",
+        videoId: "YiGkVg9oUpA",
+        ratio: "16:9",
+        groupId: 5
+    },
+    {
+        id: "6-1",
+        image_name: "",
+        videoId: "HkA7oioz4b8",
+        ratio: "16:9",
+        groupId: 6
+    },
+    {
+        id: "7-1",
+        image_name: "1loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-2",
+        image_name: "2loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-3",
+        image_name: "3loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-4",
+        image_name: "4loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-5",
+        image_name: "5loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-6",
+        image_name: "6loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-7",
+        image_name: "7loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-8",
+        image_name: "8loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-9",
+        image_name: "9loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-10",
+        image_name: "10loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "7-11",
+        image_name: "10loveinside",
+        ratio: "16:9",
+        groupId: 7
+    },
+    {
+        id: "8-1",
+        image_name: "1mercyInside",
+        ratio: "16:9",
+        groupId: 8
+    },
+    {
+        id: "8-2",
+        image_name: "2mercyInside",
+        ratio: "16:9",
+        groupId: 8
+    },
+    {
+        id: "9-1",
+        image_name: "1hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-2",
+        image_name: "2hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-3",
+        image_name: "3hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-4",
+        image_name: "4hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-5",
+        image_name: "5hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-6",
+        image_name: "6hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-7",
+        image_name: "7hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "9-8",
+        image_name: "8hateinside",
+        ratio: "16:9",
+        groupId: 9
+    },
+    {
+        id: "10-1",
+        image_name: "1jeepInside",
+        ratio: "16:9",
+        groupId: 10
+    },
+    {
+        id: "11-1",
+        image_name: "",
+        videoId: "XjAWoUc4Aj4",
+        ratio: "16:9",
+        groupId: 11
+    },
+    {
+        id: "11-2",
+        image_name: "",
+        videoId: "4txgs7qIgGo",
+        ratio: "16:9",
+        groupId: 11
+    }
+].map(media => {
+    media.src = media.videoId
+        ? `https://www.youtube.com/embed/${media.videoId}`
+        : awardsOriginal(media.image_name, media.extension);
+    return media;
+})
