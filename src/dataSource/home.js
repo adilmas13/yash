5d61a5e67091c6ff8b3a68cb38cbdb7b6e63c517
Adil Shaikh <adil@invideo.io>
2021-06-17 22:17:28 +0530

    refactor: updates home video file and timings

--- src/dataSource/home.js
+++ src/dataSource/home.js
@@ -2,17 +2,17 @@ const PAGE_NO_KEY = "PAGE_NO";
 
 export const homeVideoForwardSlots = [
     {start: 1, end: 12},
-    {start: 12, end: 24},
-    {start: 24, end: 33},
-    {start: 33, end: 56.5},
-    {start: 56.5, end: 65},
+    {start: 12, end: 23},
+    {start: 23, end: 31},
+    {start: 31, end: 52.5},
+    {start: 52.5, end: 60.55},
 ];
 
 export const homeVideoReverseSlots = [
-    {start: 65, end: 81},
-    {start: 81, end: 86},
-    {start: 86, end: 93},
-    {start: 93.5, end: 108},
+    {start: 65, end: 75.2},
+    {start: 75.2, end: 79.5},
+    {start: 79.5, end: 86.2},
+    {start: 86.2, end: 108},
 ]
 
 export const setPageNo = (pageNo) => localStorage.setItem(PAGE_NO_KEY, pageNo);
