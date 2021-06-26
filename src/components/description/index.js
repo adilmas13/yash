2061cd3459d4af0ec2331e76185d0773c543b4c5
Adil Shaikh <adil@invideo.io>
2021-06-26 21:03:21 +0530

    feat: adds scale in animation for description

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import {React} from "preact";
+import {createRef, React} from "preact";
 import style from './style.css';
+import {useEffect} from "preact/hooks";
 
 const Description = (props) => {
     const data = props.data;
     const logo = data.logo?.src || "";
+    const descCenterPieceRef = createRef();
 
     const logoStyle = {
         transform: `translateX(-50%) translateY(${data.logo?.top || 0})`,
@@ -15,9 +17,20 @@ const Description = (props) => {
         paddingTop: data.layout.paddingTop
     }
 
+    useEffect(() => {
+        const animation = descCenterPieceRef.current.animate([
+            {transform: 'scale(0.5)', opacity: 0.5},
+            {transform: 'scale(1.0)', opacity: 1},
+        ], {
+            duration: 250,
+            fill: "forwards"
+        });
+        animation.play();
+    }, [])
+
     return <div className={style['description-body']}>
         <div className={style['description-container']}>
-            <div className={style['description-center-piece']} style={layoutStyle}>
+            <div className={style['description-center-piece']} style={layoutStyle} ref={descCenterPieceRef}>
                 <div className={style['description']}>{data.description}</div>
                 <div className={style['bottom-line']} />
                 <img className={style['close']} src={'assets/cross.svg'} onClick={props.onCloseClicked} />
