beeb4022d5d2b5b01d59b70a7e978ede8f93f8fd
Adil Shaikh <adil@invideo.io>
2021-07-11 12:36:02 +0530

    feat: creates separate style for each common component

--- src/components/about-me-common-components/index.js
+++ src/components/about-me-common-components/index.js
@@ -1,25 +1,29 @@
-import style from './style.css'
+import detailsStyle from './details.css'
 import downloadStyle from './download.css'
+import headerStyle from './header.css'
 import React from "preact";
 import {cvUrl} from "../../dataSource/home";
 
-export const Header = (props) => <div className={style.header}>{props.header}</div>;
+export const Header = (props) => <div className={headerStyle.header}>{props.header}</div>;
 
-export const Details = (props) => <div className={style.details}>
-    <div className={style['title-container']}>
-        {props.data.map(it => <div className={style['title-wrapper']}>
-            <div className={style['title']}>{it.text1}</div>
-            <div className={style['symbol']}>&#8250;</div>
-        </div>)}
+export const Details = (props) => {
+    const style = detailsStyle;
+    return <div className={detailsStyle.details}>
+        <div className={style['title-container']}>
+            {props.data.map(it => <div className={style['title-wrapper']}>
+                <div className={style['title']}>{it.text1}</div>
+                <div className={style['symbol']}>&#8250;</div>
+            </div>)}
+        </div>
+        <div className={style.desc}>
+            {props.data.map(it => (
+                    it.text2 ? <span>{it.text2}{it.text3 && <span className={style.info}>{it.text3}</span>}</span> :
+                        <span>{props.children}</span>
+                )
+            )}
+        </div>
     </div>
-    <div className={style.desc}>
-        {props.data.map(it => (
-                it.text2 ? <span>{it.text2}{it.text3 && <span className={style.info}>{it.text3}</span>}</span> :
-                    <span>{props.children}</span>
-            )
-        )}
-    </div>
-</div>
+}
 
 export const Download = () => {
     const downloadCv = () => {
