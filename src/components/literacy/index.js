8230d997e9b07ac153c6ec37943cdfe007a3b1ee
Adil Shaikh <adil@invideo.io>
2021-08-09 18:44:19 +0530

    feat: css changes for literacy

--- src/components/literacy/index.js
+++ src/components/literacy/index.js
@@ -6,10 +6,6 @@ import {literacy as data} from "../../dataSource/aboutMe";
 import {Details, Download, Header} from "../about-me-common-components";
 
 const Literacy = () => <div className={style.parent}>
-    <div className={style["image-wrapper"]}>
-        <img src={aboutMeImg("literacy_shadow")} alt="yash-shadow" className={style['main-image-shadow']} />
-        <img src={aboutMeImg("literacy")} alt="yash" className={style['main-image']} />
-    </div>
     <div className={style['details-container']}>
         <div className={style.wrapper}>
             <Header header={'literacy'} />
@@ -19,6 +15,10 @@ const Literacy = () => <div className={style.parent}>
                 </Details>
             </div>
         </div>
+    </div>
+    <div className={style["image-wrapper"]}>
+        <img src={aboutMeImg("literacy_shadow")} alt="yash-shadow" className={style['main-image-shadow']} />
+        <img src={aboutMeImg("literacy")} alt="yash" className={style['main-image']} />
     </div>
 </div>
 
