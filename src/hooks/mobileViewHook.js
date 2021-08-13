aa045060323c4681dbe415d93b058e0aa4d677fc
Adil Shaikh <adil@invideo.io>
2021-08-14 00:55:43 +0530

    fix: fixes initial incorrect inner width

--- src/hooks/mobileViewHook.js
+++ src/hooks/mobileViewHook.js
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "preact/hooks";
 
 export const useIsMobileView = (breakpoint) => {
-    const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 701);
+    const [width, setWidth] = useState(0);
     useEffect(() => {
+        setWidth(window.innerWidth)
         const handleWindowResize = () => setWidth(window.innerWidth);
         window.addEventListener("resize", handleWindowResize);
         return () => window.removeEventListener("resize", handleWindowResize);
