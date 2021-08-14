2a8707e0e1efe42acc3430e2d796969eadc9f591
Adil Shaikh <adil@invideo.io>
2021-08-14 13:33:35 +0530

    feat: adds cache first in service worker

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,19 @@
 import {getFiles, setupPrecaching, setupRouting} from 'preact-cli/sw/';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 setupRouting();
+
+const handler = new CacheFirst({
+    cacheName: 'all_assets',
+    plugins: [
+        new ExpirationPlugin({
+            maxAgeSeconds: 60 * 60 * 24, // expire cache after 24 hours
+            maxEntries: 100,
+        }),
+    ],
+});
+
+registerRoute(({url}) => url['host'] ==='firebasestorage.googleapis.com', handler);
 setupPrecaching(getFiles());
